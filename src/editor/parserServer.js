b786d1628026aaf97f280f7b178cac02742d7d9c
anvaka <anvaka@gmail.com>
2021-12-21 20:11:36 -0800

    Added editor service
    First stab at code that provides autocompletion

---
+++ src/editor/parserServer.js
@@ -0,0 +1,50 @@
+var bus = require('../bus');
+
+// This is naive parser that is being used until the real `glsl-parser`
+// is loaded asynchronously. This parser assumes there are no errors
+// TODO: maybe I should be more careful here?
+var glslParser = {
+  check(code) {
+    return {
+      code,
+      log: {
+        errorCount: 0
+      }
+    };
+  }
+};
+
+let ide, inputSource;
+
+// glsl-parser is ~179KB uncompressed, we don't want to wait until it is downloaded.
+// So we load it asynchronously...
+// TODO: Replace this with a real module:
+require.ensure('my-parser/dist/src/exports/ide.js', () => {
+  // ... and replace the naive parser with the real one, when ready.
+  let {compileIDE} = require('my-parser/dist/src/exports/ide.js');
+  const {getSharedPrefix} = require('../util/shaders/getFragmentCode.js');
+  const wglAPI = require('../util/shaders/wgl2.glsl');
+
+  // TODO: Add either WebGL2 or WebGL1 to the imported tokens
+  const wgl2 = {name: 'wgl2', contents: wglAPI};
+  const shared = {name: 'shared', contents: getSharedPrefix()};
+  ide = compileIDE([wgl2, shared, {name: 'input', contents: ''} ]);
+  ide.sources.forEach(s => {
+    if (s.name === 'input') inputSource = s; 
+  });
+
+  // notify interested parties, so that they can recheck code if they wish.
+  bus.fire('glsl-parser-ready'); 
+});
+
+module.exports = function checkGLSL(code) {
+  if (ide) {
+    inputSource.tokens = null;
+    inputSource._lineOffsets = null;
+    inputSource.contents = code;
+    ide.updateState();
+    return ide;
+  }
+
+  return glslParser.check(code);
+}
---
+++ src/monaco-services/createEditor.js
@@ -0,0 +1,189 @@
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api.js';
+import {glslMonacoLanguage} from './language.js'
+import {debounce} from '../util/utils.js';
+
+var eventify = require('ngraph.events');
+const checkGLSL = require('../editor/parserServer.js');
+
+const CompletionKindToSuggestionKind = {
+  variable: monaco.languages.CompletionItemKind.Variable,
+  function: monaco.languages.CompletionItemKind.Function,
+  struct: monaco.languages.CompletionItemKind.Struct,
+  keyword: monaco.languages.CompletionItemKind.Keyword
+};
+
+monaco.languages.register({ id: 'glsl' });
+monaco.languages.setLanguageConfiguration('glsl', glslMonacoLanguage.configuration);
+monaco.languages.setMonarchTokensProvider('glsl', glslMonacoLanguage.tokenProvider);
+
+monaco.languages.registerCompletionItemProvider('glsl', {
+  triggerCharacters: ['.'],
+  provideCompletionItems(model, position) {
+    const contents = model.getValue();
+    let res = checkGLSL(contents);
+
+    let q = res.completionQuery({
+      source: 'input',
+      line: position.lineNumber - 1,
+      column: position.column - 1,
+    });
+
+    return {
+      suggestions: q.completions.map(c => {
+        return {
+          label: c.name,
+          kind: CompletionKindToSuggestionKind[c.kind],
+          insertText: c.name,
+          detail: c.detail,
+          documentation: {
+            value: c.documentation
+          }
+        }
+      })
+    };
+  }
+})
+
+monaco.languages.registerHoverProvider('glsl', {
+  provideHover(model, position) {
+    const contents = model.getValue();
+    let compiled = checkGLSL(contents);
+    let response = compiled.tooltipQuery({
+      ignoreDiagnostics: true,
+      source: 'input',
+      line: position.lineNumber - 1,
+      column: position.column - 1,
+    });
+    if (response.tooltip !== null && response.range !== null) {
+      return {
+        contents: [
+          {value: '```glsl\n' + response.tooltip + '\n```'},
+          {value: response.documentation}
+        ],
+        range: convertToRange(response.range)
+      }
+    }
+    return {contents: []};
+  }
+});
+
+function convertToRange(range) {
+  return {
+    startLineNumber: range.start.line + 1,
+    startColumn: range.start.column + 1,
+    endLineNumber: range.end.line + 1,
+    endColumn: range.end.column + 1,
+  }
+}
+
+monaco.languages.registerDefinitionProvider('glsl', {
+  provideDefinition(model, position) {
+    const contents = model.getValue();
+
+    let compiled = checkGLSL(contents);
+    let response = compiled.definitionQuery({
+      source: 'input',
+      line: position.lineNumber - 1,
+      column: position.column - 1,
+    });
+    if (response.definition === null || response.definition.source !== 'input') {
+      return []
+    }
+    return {
+      uri: response.definition.source,
+      range: convertToRange(response.definition),
+    };
+  }
+})
+
+monaco.editor.defineTheme('pplay-blue', {
+  base: 'vs-dark',
+  inherit: true,
+  rules: [],
+  colors: {'editor.background': '#061838'}
+});
+
+export function createEditor(container, code) {
+  let hasErrors = false;
+  let changedContent = false;
+  let validationPerformed = false;
+
+  let editor = monaco.editor.create(container, {
+    value: code,
+    automaticLayout: true,
+    fixedOverflowWidgets: true,
+    language: 'glsl',
+    theme: 'pplay-blue',
+    fontSize: 14,
+    minimap: {
+      enabled: false,
+    },
+  });
+  // Don't hijack cmd + l (got to browser's input field)
+  editor._standaloneKeybindingService.addDynamicKeybinding('-expandLineSelection', null, Function.prototype);
+
+  // Hack to make suggestion details visible by default:
+  let {widget} = editor.getContribution('editor.contrib.suggestController');
+  if (widget) {
+    const suggestWidget = widget.value;
+    if (suggestWidget && suggestWidget._setDetailsVisible) {
+      suggestWidget._setDetailsVisible(true);
+    }
+  }
+
+  editor.getModel().onDidChangeContent(() => {
+    changedContent = true;
+    validationPerformed = false;
+    debounce(() => validate(editor.getModel()), 500);
+  })
+  
+  let api = {
+    dispose,
+    editor,
+    getValue,
+    setValue
+  }
+
+  eventify(api);
+
+  return api;
+
+  function getValue() {
+    return editor.getModel().getValue();
+  }
+
+  function setValue(code) {
+    editor.getModel().setValue(code);
+  }
+
+  function validate(model) {
+    const contents = model.getValue();
+    let res = checkGLSL(contents);
+    let markers = res.diagnostics.filter(d => d.range.source === 'input').map(d => {
+      return {
+        severity: monaco.MarkerSeverity.Error,
+        startLineNumber: d.range.start.line + 1,
+        startColumn: d.range.start.column + 1,
+        endLineNumber: d.range.end.line + 1,
+        endColumn: d.range.end.column + 1,
+        message: d.text
+      }
+    })
+
+    validationPerformed = true;
+    hasErrors = markers.length > 0;
+    notifyContentChanged();
+    monaco.editor.setModelMarkers(model, 'glsl', markers);
+  }
+
+  function notifyContentChanged() {
+    if (!hasErrors && validationPerformed && changedContent){
+      api.fire('content-changed', editor, hasErrors);
+      changedContent = false;
+    }
+  }
+
+  function dispose() {
+    editor.dispose();
+  }
+}
